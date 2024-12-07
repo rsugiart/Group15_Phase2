a3d0a7f6fd19c68981a2361c24611b11ac5a6a4d
Varun <varunvenkatesh3@gmail.com>
2024-12-07 16:34:41 -0500

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,11 +13,15 @@ import Login from './pages/Login/Login';
 import AdminPage from './pages/Admin/Admin';
 import CreateUserPage from './pages/Admin/CreateUser/CreateUser';
 import ModifyUsersPage from './pages/Admin/ModifyUserPermissions/ModifyUserPermissions';
+import ProtectedRoute from './components/useAuth';
 
 function App() {
   const [token, setToken] = useState<string>('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = useState<string[]>(['Admin']);
+  const [permissions, setPermissions] = useState<string[]>(['upload', 'download', 'search']);
+  setToken(localStorage.getItem('accessToken') || '');
+  setPermissions(localStorage.getItem('permissions')?.split(',') || []);
   
   return (
     <Router>
@@ -28,14 +32,39 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/get-started" element={<Get_Started />} />
-        <Route path="/search" element={<Search token={token} />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/download" element={<Download />} />
-        {/* <Route path="/rate" element={<Rate />} /> */}
-        <Route path="/login" element={<Login token={token} setToken = {setToken} isLoggedIn={isLoggedIn} setIsAdmin={setIsAdmin} 
-        isAdmin={isAdmin} setIsLoggedIn={setIsLoggedIn}/>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/login" element={<Login setPermissions={setPermissions} setIsAdmin={setIsAdmin}/>} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute permissions={isAdmin} permission='Admin'>
+              <AdminPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/upload"
+          element={
+            <ProtectedRoute permissions={permissions} permission='upload'>
+              <AdminPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute permissions={permissions} permission='download'>
+              <AdminPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/download"
+          element={
+            <ProtectedRoute permissions={permissions} permission='search'>
+              <AdminPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/admin/create-user" element={<CreateUserPage />} />
         <Route path="/admin/modify-user-permissions" element={<ModifyUsersPage />} />
         <Route path="*" element={<h1>Not Found</h1>} />
