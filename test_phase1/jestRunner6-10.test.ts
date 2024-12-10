e75e0be89cd922e9cec96fbf8cffea876068eb77
Ruth Sugiarto <122235589+rsugiart@users.noreply.github.com>
2024-12-09 23:53:52 -0500

    Update jestRunner6-10.test.ts

--- test_phase1/jestRunner6-10.test.ts
+++ test_phase1/jestRunner6-10.test.ts
@@ -1,3 +1,4 @@
+/*Set up Phase 1 Jest Test Runner for test 6-10*/
 import { runCommand } from '../rating/jestRunner';
 import { describe, it, expect, afterAll } from '@jest/globals';
 import * as fs from 'fs';
